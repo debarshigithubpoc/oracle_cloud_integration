4e0c4acc1c46f734c83845a4a5a7cebd1c6c960f
Debarshi Chakraborty <64696021+debarshigithubpoc@users.noreply.github.com>
2024-12-27 16:55:38 +0530

    fixed

--- pipeline/terraform_deploy_dev.pl
+++ pipeline/terraform_deploy_dev.pl
@@ -25,6 +25,7 @@ pipeline {
             }
             steps {
                 bat """
+                    if exist "%WORKSPACE_DIR%" rmdir /s /q "%WORKSPACE_DIR%"
                     cd "%WORKSPACE_DIR%"
                     git clone %GITHUB_REPO% 
                 """
