eba615f0c083e511feb77e5cd15e9209788764e8
Debarshi Chakraborty <64696021+debarshigithubpoc@users.noreply.github.com>
2024-12-29 21:51:54 +0530

    fixed

--- pipeline/docker_build_dotnet_template.pl
+++ pipeline/docker_build_dotnet_template.pl
@@ -1,3 +1,3 @@
 // call(String branchName, String dockerDirectory, String agentLabel, String dockerRegistrySecret , String dockerUsernameSecret, String dockerPasswordSecret)
 @Library('dockerPipelineTemplate') _
-pipelineTemplate('development', 'Applications/DotnetDocker/dotnethelloworld', 'linux', 'dockerRegistry', 'dockerUsername', 'dockerPassword' )
+dockerPipelineTemplate('development', 'Applications/DotnetDocker/dotnethelloworld', 'linux', 'dockerRegistry', 'dockerUsername', 'dockerPassword' )
