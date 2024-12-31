7aa38a0a20861d62af375ca16f17e814c278fae3
Debarshi Chakraborty <64696021+debarshigithubpoc@users.noreply.github.com>
2024-12-31 18:00:00 +0530

    Update docker_build_java.pl

--- pipeline/docker_build_java.pl
+++ pipeline/docker_build_java.pl
@@ -1,4 +1,5 @@
 // Jenkinsfile for Docker Build for java
 // variablesFormat(String branchName, String dockerDirectory, String dockerImageName, String agentLabel, String dockerRegistrySecret , String dockerUsernameSecret, String dockerPassSecret)
 @Library('dockerPipelineTemplate') _
-dockerPipelineTemplate('development', 'Applications/JavaDocker', 'java', 'linux', 'dockerRegistry', 'dockerUsername', 'dockerPassword' )
+dockerPipelineTemplate('development', 'Applications/JavaDocker', 'java', 'linux', 'dockerRegistry', 'dockerUsername', 'dockerPassword', 'javahellorelease', './javahelloworld/')
+
